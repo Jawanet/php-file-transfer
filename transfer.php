af1b42315f5ebb310ae18f9241db98ef1394a65b
Wp Dev <indteamhost@gmail.com>
2023-01-25 16:45:45 +0700

    Update transfer.php

--- transfer.php
+++ transfer.php
@@ -4,14 +4,14 @@ set_time_limit(0); //Unlimited max execution time
 $path = 'newfile.zip';
 $url = 'http://example.com/oldfile.zip';
 $newfname = $path;
-echo 'Starting Download!<br>';
+echo 'Starting Download File!<br>';
 $file = fopen ($url, "rb");
 if($file) {
 	$newf = fopen ($newfname, "wb");
 	if($newf)
 		while(!feof($file)) {
 			fwrite($newf, fread($file, 1024 * 8 ), 1024 * 8 );
-			echo '1 MB File Chunk Written!<br>';
+			echo '1 MB File transferred!<br>';
 		}
 }
 if($file) {
